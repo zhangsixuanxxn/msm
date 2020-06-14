48294833a0517bee70328fc167630b0b29062d30
pan.zhang <pan.zhang@capitalonline.net>
2020-06-14 13:48:26 +0800

    msm

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
   </div>
 </template>
 
-<style >
-  body {
-    font-family: "微软雅黑";
-    margin: 0px auto;
-  }
-</style>
+<!--<style >-->
+  <!--body {-->
+    <!--font-family: "微软雅黑";-->
+    <!--margin: 0px auto;-->
+  <!--}-->
+<!--</style>-->
 
